34f5a4cc567194bce2b43d3a97069854404e55f9
Rafael Antunes <rafaelmessias710@gmail.com>
2022-07-05 08:41:00 -0400

    refactor: change to functional component

--- src/App.js
+++ src/App.js
@@ -1,100 +1,103 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import WebRTCClient from 'freepbx-react-webrtc'
+import EventEmitter from 'events'
+import Button from 'react-bootstrap/Button'
 
-import WebRTCClient from "freepbx-react-webrtc";
+import './App.css'
 
-import EventEmitter from "events";
-import Button from 'react-bootstrap/Button';
+export default function NewApp() {
+  const eventHandler = React.useMemo(() => new EventEmitter(), [])
+  const [currentCallState, setCurrentCallState] = React.useState(null)
+  const [currentConnectionState, setCurrentConnectionState] =
+    React.useState(null)
 
-function App() {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-        eventHandler: new EventEmitter(),
-        currentCallState: null,
-        currentConnectionState: null
-    };
+  const eventHandlerEmit = (emit) => {
+    eventHandler.emit(emit)
   }
 
-  componentDidMount() {
+  const updateCallState = (callState) => {
+    setCurrentCallState(callState)
+  }
+
+  const updateConnectionState = (connectionState) => {
+    setCurrentConnectionState(connectionState)
+  }
+
+  React.useEffect(() => {
     // Connected and Idle
-    this.state.eventHandler.on("Idle", function() {
+    eventHandler.on('Idle', function () {
       // Do something...
-    });
+    })
 
     // Outgoing call happening
-    this.state.eventHandler.on("Calling", function() {
+    eventHandler.on('Calling', function () {
       // Do something...
-    });
+    })
 
     // Incoming Call
-    this.state.eventHandler.on("Alerting", function() {
+    eventHandler.on('Alerting', function () {
       // Do something...
-    });
+    })
 
     // Call started
-    this.state.eventHandler.on("InCall", function() {
+    eventHandler.on('InCall', function () {
       // Do something...
-    });
-  };
-
-  eventHandlerEmit = (emit) => {
-    this.state.eventHandler.emit(emit);
-  }
-
-  updateCallState = (callState) => {
-    this.setState({
-      ...this.state,
-      currentCallState: callState
     })
-  }
-
-  updateConnectionState = (connectionState) => {
-    this.setState({
-      ...this.state,
-      currentConnectionState: connectionState
-    })
-  }
+  }, [eventHandler])
 
   return (
     <div className="App">
-      <header>
-
-      </header>
+      <header></header>
       <body>
-        <video width="25%" height="25%" id="localVideo" autoPlay playsInline  muted="muted"></video>
-        <video width="50%" height="50%" id="remoteVideo" autoPlay playsInline ></video>
+        <video
+          width="25%"
+          height="25%"
+          id="localVideo"
+          autoPlay
+          playsInline
+          muted="muted"
+        ></video>
+        <video
+          width="50%"
+          height="50%"
+          id="remoteVideo"
+          autoPlay
+          playsInline
+        ></video>
 
-        <Button onClick={() => this.eventHandlerEmit("answerCall")}>Accept Incoming Call</Button>
-        <Button onClick={() => this.eventHandlerEmit("hangupCall")}>Hangup Call</Button>
-        <Button onClick={() => this.eventHandlerEmit("placeCall")}>Start Call</Button>
+        <Button onClick={() => this.eventHandlerEmit('answerCall')}>
+          Accept Incoming Call
+        </Button>
+        <Button onClick={() => this.eventHandlerEmit('hangupCall')}>
+          Hangup Call
+        </Button>
+        <Button onClick={() => this.eventHandlerEmit('placeCall')}>
+          Start Call
+        </Button>
 
         <WebRTCClient
-            enableVideo={true}
-            enableSound={true}
-            webSocketPort={"8089"} //default port for websocket is 8089 on sip server
-            autoRegister = {true}
-            sipDomain={"sip.domain.de"} // Sip Domain
-            sipServer={"123.123.2.12"} // Optional: Sip Server Adress, if not set domain is used instead
-            sipUser={"1234"} // Username of caller
-            sipPassword={"SIP_CALLER_PASS"} // Password of caller
-            destination={"1234@destination.url"} // Destination, user@destination.domain
-            metaData={{}} // Metadata
-            alertVideoUrl={"alertUrl"}
-            ringbackVideoUrl={"ringbackUrl"}
-            localVideoTagId={"localVideo"} // ID of video tag for own video
-            remoteVideoTagId={"remoteVideo"} // ID of video tag for incoming remote video
-            skipStunServer={true} // True if Server is only reachable in local network and is not public
-            eventHandler={this.state.eventHandler}
-            eventHandlerEmit={this.eventHandlerEmit}
-            updateCallState={this.updateCallState}
-            updateConnectionState={this.updateConnectionState}
-            traceSip={true}
+          // enableVideo={true}
+          enableSound={true}
+          webSocketPort={'8089'} //default port for websocket is 8089 on sip server
+          autoRegister={true}
+          sipDomain={'sip.domain.de'} // Sip Domain
+          sipServer={'123.123.2.12'} // Optional: Sip Server Adress, if not set domain is used instead
+          sipUser={'1234'} // Username of caller
+          sipPassword={'SIP_CALLER_PASS'} // Password of caller
+          destination={'1234@destination.url'} // Destination, user@destination.domain
+          metaData={{}} // Metadata
+          alertVideoUrl={'alertUrl'}
+          ringbackVideoUrl={'ringbackUrl'}
+          localVideoTagId={'localVideo'} // ID of video tag for own video
+          remoteVideoTagId={'remoteVideo'} // ID of video tag for incoming remote video
+          skipStunServer={true} // True if Server is only reachable in local network and is not public
+          eventHandler={eventHandler}
+          eventHandlerEmit={eventHandlerEmit}
+          updateCallState={updateCallState}
+          updateConnectionState={updateConnectionState}
+          traceSip={true}
         />
       </body>
     </div>
-  );
+  )
 }
-
-export default App;
